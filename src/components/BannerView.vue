063d2df3b1e7365c38102efed470d6add3228934
YouyuLisng <a0979534311@gmail.com>
2022-07-13 01:46:23 +0800

    ok

--- src/components/BannerView.vue
+++ src/components/BannerView.vue
@@ -3,7 +3,7 @@
        <div class="mask"></div>
        <div class="position-absolute top-50 start-50 translate-middle ">
          <h3 class="text-white text-center lh-base animate__animated animate__fadeInDown animate__delay-1s">假日，不必去遠方度假<br>在家即可享受假期的放鬆</h3>
-         <router-link type="button" class="btn btn-warning position-absolute top-100 start-50 translate-middle mt-5 animate__animated animate__zoomIn animate__delay-1s" to="/cart">選購去</router-link>
+         <router-link type="button" class="btn btn-warning position-absolute top-100 start-50 translate-middle mt-5 animate__animated animate__zoomIn animate__delay-1s" to="/user/cart">選購去</router-link>
        </div>
     </div>
 </template>
