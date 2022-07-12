b5d38f02b3721a71cb64b4c692ebfdba918995e8
YouyuLisng <a0979534311@gmail.com>
2022-07-12 16:55:38 +0800

    q

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('../views/UserView.vue'),
+    component: () => import('../views/HomeUserView.vue'),
     children: [
       {
         path: 'frontpage',
